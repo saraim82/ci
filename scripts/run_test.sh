e96b9ed6ec617a0ae7499e8e8b1c4565cbe79041
Sarai Morales <Sarai_Morales@ultimatesoftware.com>
2018-01-19 17:29:43 -0500

    test

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -3,8 +3,8 @@
 set -e # fail fast
 set -x # print commands
 
-apk update && apk add bash
-apt-get -yqq install docker.io
+#apk update && apk add bash
+#apt-get -yqq install docker.io
 
 cd resource-carts
 ./test/test.sh unit.py
