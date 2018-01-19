d95da29d2d7c52ddb0974eb328ccd1bf92c3d0e7
Sarai Morales <Sarai_Morales@ultimatesoftware.com>
2018-01-19 17:32:18 -0500

    test

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -6,7 +6,7 @@ set -x # print commands
 #apk update && apk add bash
 #apt-get -yqq install docker.io
 
-RUN apt-get update && apt-get install -y sudo && rm -rf /var/lib/apt/lists/*
+apt-get update && apt-get install -y sudo && rm -rf /var/lib/apt/lists/*
 
 cd resource-carts
 ./test/test.sh unit.py
